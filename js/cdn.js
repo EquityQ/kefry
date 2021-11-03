6200fda194e021a12b3b844cfab5b20480b9237e
EquityQ <87411060+EquityQ@users.noreply.github.com>
2021-11-03 18:05:50 +0800

    Add files via upload

---
+++ js/cdn.js
@@ -0,0 +1,26 @@
+function delete_it(obj){
+    document.getElementById(obj).innerHTML='';
+}
+setTimeout('delete_it("cdn_mid_p")',6000);
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+c = 0;
+function lined(){
+    var num = getRndInteger(1,10);
+    c = c + num;
+    var o = c / 100;
+    if (o>1){o=1;}
+    var u = 40 * o;
+    var objs = document.getElementById('linesksks');
+    var k = parseInt(u);
+    objs.style.width=k+'vw';
+    var t = c;
+    if (t>100){
+        t=100;
+        //setTimeout('window.location.href="/url/index.html"',2000);
+    }
+    var r = parseInt(t);
+    document.getElementById('percent').innerHTML=r+'%';
+}
+setTimeout('setInterval("lined()",500);',8000);
