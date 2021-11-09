407c410090d7e5e9719fdfe9e39b5d4208cfa37b
EquityQ <87411060+EquityQ@users.noreply.github.com>
2021-11-09 17:23:25 +0800

    Add files via upload

---
+++ js/pc_mobile_href.js
@@ -0,0 +1,6 @@
+if(navigator.userAgent.match(/(iPhone|iPod|Android|ios)/i)){
+    window.location.href="https://equityq.github.io/kefry/m";
+}
+else{
+    window.location.href="https://equityq.github.io/kefry/#";
+}
